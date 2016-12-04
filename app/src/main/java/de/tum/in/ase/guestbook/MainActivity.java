b058d1807cf30eee8578ea4a15c168e372803315
Benedikt Schlagberger <schlagbe@in.tum.de>
2016-12-04 18:05:22 +0100

    implementing basic version with NetworkOnUIThreadException

--- app/src/main/java/de/tum/in/ase/guestbook/MainActivity.java
+++ app/src/main/java/de/tum/in/ase/guestbook/MainActivity.java
@@ -1,7 +1,15 @@
 package de.tum.in.ase.guestbook;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
+import org.restlet.resource.ClientResource;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +17,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.guestbook_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_item_download:
+                downloadGuestbook();
+            default:
+                return false;
+        }
+    }
+
+    private void downloadGuestbook() {
+        String url = "http://ase2016-148507.appspot.com/rest/guestbook/";
+        try {
+            String rawAnswer = new ClientResource(url).get().getText();
+            TextView textView = (TextView) findViewById(R.id.text_view);
+            textView.setText(rawAnswer);
+        } catch (IOException e) {
+            Toast.makeText(this.getApplicationContext(),
+                    "Could not download guestbook", Toast.LENGTH_SHORT).show();
+        }
     }
 }
